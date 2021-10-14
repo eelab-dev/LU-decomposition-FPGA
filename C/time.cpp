f6f37fd61c13046e7fa2dfd76b0c119c8e9682c9
s2130520 <s2130520@ed.ac.uk>
2021-10-14 23:53:32 +0100

    Moved

--- C/time.cpp
+++ C/time.cpp
@@ -9,7 +9,7 @@ int main()
     for (int i = 1; i < 1000; i++)
         x += i;
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[\u03BCs]" << std::endl;
+    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[μs]" << std::endl;
     std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count() << "[ns]" << std::endl;
     cout << x;
     return 0;
